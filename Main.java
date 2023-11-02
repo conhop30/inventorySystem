3ec53a13526f4e538568911e4060afff953abb56
Connor Hopkins <conhop22@yahoo.com>
2023-11-01 20:04:31 -0700

    "Hello, World" runs.

---
+++ Main.java
@@ -0,0 +1,18 @@
+
+/*********************************************
+ Program: Inventory System
+ Author: Connor Hopkins
+ Purpose: This program will do the following--
+    - Add things to the inventory
+    - Remove things from the inventory
+    - Move items around the inventory
+ Time logged:
+ 7:45pm - 
+ *********************************************/
+
+// Driver of the program
+public class Main {
+  public static void main(String[] args) {
+    System.out.println("Hello World");
+  }
+}
